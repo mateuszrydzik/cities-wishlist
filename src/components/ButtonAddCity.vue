556c71379327b76d3827e5f997cfe307ac4029a8
mateuszrydzik <panmisterek@outlook.com>
2022-04-28 14:25:45 +0200

    added button that will later add new markers

---
+++ src/components/ButtonAddCity.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="button">
+        <button @click="logMessage">+</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'ButtonAddCity',
+    methods: {
+        logMessage() {
+            console.log('Gratulacje uzytkowniku')
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+  background-color: #4CAF50;
+  font-size: 30px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  height: 75px;
+  width: 75px;
+  border-radius: 50%;
+}
+</style>
