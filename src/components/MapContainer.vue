80655cd6e7afa4430d89bde032119e37a4a47a01
mateuszrydzik <panmisterek@outlook.com>
2022-05-17 14:56:30 +0200

    removed unused methods, onclicks

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -1,7 +1,7 @@
 <template>
-  <div ref="map-root"
-       style="width: 100%; height: 100%"
-       @click="GeolocationCoordinates">
+  <div 
+    ref="map-root"
+    :style="{ cursor: $store.state.cursor }">
   </div>
 </template>
 
@@ -9,6 +9,7 @@
   import View from 'ol/View'
   import Map from 'ol/Map'
   import TileLayer from 'ol/layer/Tile'
+  import Vector from 'ol/layer/Vector'
   import OSM from 'ol/source/OSM'
 
   // importing the OpenLayers stylesheet is required for having
@@ -21,7 +22,7 @@
     name: 'MapContainer',
     mounted() {
       // this is where we create the OpenLayers map
-      new Map({
+      const map = new Map({
         // the map will be created using the 'map-root' ref
         target: this.$refs['map-root'],
         layers: [
@@ -29,6 +30,9 @@
           new TileLayer({
             source: new OSM() // tiles are served by OpenStreetMap
           }),
+          new Vector({
+            
+          })
         ],
 
         // the map view will initially show the whole world
@@ -41,17 +45,14 @@
       })
     },
     methods: {
-      reverseGeocode(coords) {
-      fetch('http://nominatim.openstreetmap.org/reverse?format=json&lon=' + coords[0] + '&lat=' + coords[1])
-      .then(function(response) {
-            return response.json();
-        }).then(function(json) {
-            console.log(json);
-        });
-      },
-      getCoords() {
-         console.log(this.map.getView().getCenter())
-      }
   }
 }
 </script>
+
+<style scoped>
+div {
+  width:100%;
+  height:calc(100% - 60px);
+  position:fixed;
+}
+</style>
