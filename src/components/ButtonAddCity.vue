d61ecedf34dae21032ed33cc1ef8f6fbdefc4951
mateuszrydzik <panmisterek@outlook.com>
2022-05-17 14:54:16 +0200

    changed to vuetify

--- src/components/ButtonAddCity.vue
+++ src/components/ButtonAddCity.vue
@@ -1,35 +1,28 @@
 <template>
-    <div id="button">
-        <v-container>
-            <v-btn 
-            icon 
-            elevation="10"
-            color="secondary" 
-            @click="enterEditMode"> 
-                {{ icon }} 
-            </v-btn>
-        </v-container>
-    </div>
+  <div id="button">
+    <v-container>
+      <v-btn 
+        icon 
+        elevation="10"
+        color="green" 
+        @click="$store.dispatch('getRevgeocode')"> 
+          {{ $store.state.icon }} 
+      </v-btn>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default{
-    name: 'ButtonAddCity',
-    data() {
-        return {
-            editModeIsActive: false,
-            icon: '+'
-        }
-    },
-    methods: {
-        enterEditMode() {
-            this.editModeIsActive = !this.editModeIsActive;
-            this.icon = this.editModeIsActive ? 'edit' : '+';
-        }
-    } 
+  name: 'ButtonAddCity',
 }
 </script>
 
 <style scoped>
-
+#button {
+    position: fixed;
+    right: 5%;
+    bottom: 5%;
+    z-index: 1;
+}
 </style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-toolbar
+    app
+    dark
+    color="green"
+    height="60px"
+    >
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+export default{
+    name: 'Toolbar',
+    props: {
+        title: String
+    }
+}
+</script>
