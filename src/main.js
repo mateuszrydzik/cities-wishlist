224d4bc4c06fd8b3a5abb68c423bbb08ae2bf12f
mateuszrydzik <panmisterek@outlook.com>
2022-05-17 14:55:27 +0200

    added vuex

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import App from './App.vue'
 import { loadFonts } from './plugins/webfontloader'
-
+import store from "./store/index.js"
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -15,5 +15,7 @@ const vuetify = createVuetify({
   directives,
 })
 
+
 app.use(vuetify)
+app.use(store)
 app.mount('#app')
