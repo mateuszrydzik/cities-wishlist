f12402ba5a11809fc3fd232240b554e38199d188
mateuszrydzik <panmisterek@outlook.com>
2022-06-13 14:04:42 +0200

    implemented draw interaction with reverse geocoding feature

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -1,86 +1,98 @@
 <template>
-  <div 
-    ref="map-root"
-    :style="{ cursor: $store.state.cursor }">
-  </div>
+  <div ref="map-root" :style="{ cursor: $store.state.cursor }"></div>
 </template>
 
 <script>
-  import View from 'ol/View'
-  import Map from 'ol/Map'
-  import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer'
-  import {OSM, Vector as VectorSource } from 'ol/source';
-	import {Draw} from 'ol/interaction';
-  import {fromLonLat} from 'ol/proj';
-  // importing the OpenLayers stylesheet is required for having
-  // good looking buttons!
-  import 'ol/ol.css'
+import View from "ol/View";
+import Map from "ol/Map";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { OSM, Vector as VectorSource } from "ol/source";
+import { Draw } from "ol/interaction";
+import { fromLonLat, transform } from "ol/proj";
+// importing the OpenLayers stylesheet is required for having
+// good looking buttons!
+import "ol/ol.css";
 
-  export default {
-    name: 'MapContainer',
-    computed: {
-      watchEditMode(){
-        return this.$store.getters.editModeIsActiveState
-      }
+export default {
+  name: "MapContainer",
+  data: () => ({
+    active: false,
+    map: undefined,
+  }),
+  computed: {
+    watchEditMode() {
+      return this.$store.getters.editModeIsActiveState;
     },
-    mounted() {
-      const raster = new TileLayer({
-            source: new OSM()
-          });
-			const source = new VectorSource();
-      const vector = new VectorLayer({
-        source:source
-      });
-      const map = new Map({
-        target: this.$refs['map-root'],
-        layers: [raster, vector],
-        view: new View({
-          zoom: 4,
-          projection: 'EPSG:3857',
-          center: fromLonLat([15, 52]),
-          constrainResolution: true
-        }),
+  },
+  methods: {
+    addDrawInteraction() {
+      const vectorLayer = new VectorLayer({
+        source: new VectorSource(),
       });
       const draw = new Draw({
-        type:'Point',
-        source:source
+        type: "Point",
+        source: vectorLayer.getSource(),
       });
-      this.addMap(map,draw) //dziala
+      draw.on("drawend", (event) => {
+        const coordinates = transform(
+          event.feature.getGeometry().getCoordinates(),
+          "EPSG:3857",
+          "EPSG:4326"
+        );
+        console.log(coordinates);
+        this.$store.dispatch("getRevgeocode", coordinates.reverse());
+        this.toggleEditMode();
+      });
+      draw.setActive(false);
+      draw.set("name", "drawInteraction");
+      this.map.addLayer(vectorLayer);
+      this.map.addInteraction(draw);
     },
-    methods: {
-      addMap(map, draw){
-        map.addInteraction(draw)
-      },
-      changeDraw(state, map, draw){
-        if (state===true){
-          map.addInteraction(draw)
-          } else if (state===false){
-          map.removeInteraction(draw)
-          }
-        },
-        testLog(state){
-          console.log(`current state: ${state}`)
-        }
-			},
-    watch: {
-      watchEditMode: {
-        handler: function(newState, oldState){
-          // this.testLog(newState) poprawnie loguje, this z metodami dziala
-          this.changeDraw(newState, this.map, this.draw) 
-          //TypeError: undefined is not an object
-          //map i draw jest undefined w watch
-          }
+    toggleEditMode() {
+      this.$store.commit("enterEditMode");
+    },
+    getInteractionByName(interactionName) {
+      let interaction = undefined;
+      for (const i of this.map.getInteractions().getArray()) {
+        if (i.get("name") === interactionName) {
+          interaction = i;
         }
       }
-    }
-
+      return interaction;
+    },
+  },
+  watch: {
+    watchEditMode: {
+      handler: function (newState, oldState) {
+        const interaction = this.getInteractionByName("drawInteraction");
+        interaction.setActive(newState);
+      },
+    },
+  },
+  mounted() {
+    this.map = new Map({
+      target: this.$refs["map-root"],
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        zoom: 4,
+        projection: "EPSG:3857",
+        center: fromLonLat([15, 52]),
+        constrainResolution: true,
+      }),
+    });
+    this.addDrawInteraction();
+  },
+};
 </script>
 
 <style scoped>
 div {
-  width:100%;
-  height:calc(100% - 60px);
-  position:fixed;
+  width: 100%;
+  height: calc(100% - 60px);
+  position: fixed;
 }
 </style>
-
