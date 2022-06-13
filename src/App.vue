dfdffaeb2e979d42d132107fd4cbf1f8b4d1f168
mateuszrydzik <panmisterek@outlook.com>
2022-06-13 16:29:38 +0200

    imported modal

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,48 @@
 <template>
-	<v-app>
-		<div id="app">
-			<Toolbar :title="title"/>          
-			<MapContainer />
-			<ButtonAddCity />
-		</div>
-	</v-app>
+  <v-app>
+    <div id="app">
+      <Toolbar :title="title" />
+      <MapContainer />
+      <ButtonAddCity />
+      <Modal></Modal>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import Toolbar from './components/Toolbar.vue'
-import ButtonAddCity from './components/ButtonAddCity.vue'
-import MapContainer from './components/MapContainer.vue'
+import Toolbar from "./components/Toolbar.vue";
+import ButtonAddCity from "./components/ButtonAddCity.vue";
+import MapContainer from "./components/MapContainer.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
-    name: 'App',
-    components: {
-        Toolbar,
-        ButtonAddCity,
-        MapContainer
-    },
-    data() {
-        return {
-            title: 'Cities Wishlist',
-            date_set: 'when its done'
-        }
-    }
-}
+  name: "App",
+  components: {
+    Toolbar,
+    ButtonAddCity,
+    MapContainer,
+    Modal,
+  },
+  data() {
+    return {
+      title: "Cities Wishlist",
+    };
+  },
+};
 </script>
 
 <style>
-html, body {
-    height: 100%;
-    margin: 0;
+html,
+body {
+  height: 100%;
+  margin: 0;
 }
 #app {
-    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-    height: 100%;
-    width: 100%;
-    top: 0;
-    left: 0;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
 }
 </style>
