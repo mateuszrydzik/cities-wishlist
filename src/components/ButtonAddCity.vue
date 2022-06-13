92faa4562af120e1f4c620efa0cf18252ac5bede
mateuszrydzik <panmisterek@outlook.com>
2022-06-13 14:04:06 +0200

    added local icon for button

--- src/components/ButtonAddCity.vue
+++ src/components/ButtonAddCity.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="button">
     <v-container>
-      <v-btn 
-        icon 
+      <v-btn
+        icon
         elevation="10"
-        color="green" 
-        @click="$store.commit('enterEditMode')"> 
-          {{ $store.state.icon }} 
+        color="green"
+        @click="$store.commit('enterEditMode')"
+      >
+        {{ icon }}
       </v-btn>
     </v-container>
   </div>
 </template>
 
 <script>
-export default{
-  name: 'ButtonAddCity',
-}
+export default {
+  name: "ButtonAddCity",
+  data: () => ({
+    icon: "+",
+  }),
+};
 </script>
 
 <style scoped>
 #button {
-    position: fixed;
-    right: 5%;
-    bottom: 5%;
-    z-index: 1;
+  position: fixed;
+  right: 5%;
+  bottom: 5%;
+  z-index: 1;
 }
 </style>
