9beb74534ce8e6a1a269b0edc3177b740172ae2c
mateuszrydzik <panmisterek@outlook.com>
2022-06-15 16:21:33 +0200

    added city and country name to vector layer feature, added labels

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -1,8 +1,10 @@
 <template>
   <div ref="map-root" :style="{ cursor: $store.state.cursor }"></div>
+  <div id="modal" ref="modal"><Modal /></div>
 </template>
 
 <script>
+import Modal from "./Modal.vue";
 import View from "ol/View";
 import Map from "ol/Map";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
@@ -10,10 +12,14 @@ import { OSM, Vector as VectorSource } from "ol/source";
 import { Draw } from "ol/interaction";
 import { fromLonLat, transform } from "ol/proj";
 import Overlay from "ol/Overlay";
+import { Style, Text, Fill, Stroke, Circle } from "ol/style";
 import "ol/ol.css";
 
 export default {
   name: "MapContainer",
+  components: {
+    Modal,
+  },
   data: () => ({
     active: false,
     map: undefined,
@@ -21,6 +27,9 @@ export default {
   computed: {
     watchEditMode() {
       return this.$store.getters.editModeIsActiveState;
+    },
+    cityName() {
+      return this.$store.state.city;
     },
   },
   methods: {
@@ -32,24 +41,47 @@ export default {
         type: "Point",
         source: vectorLayer.getSource(),
       });
-      // const overlay = new Overlay({
-      //   element: this.$refs["modal"],
-      // });
+      const overlay = new Overlay({
+        element: this.$refs["modal"],
+      });
       draw.on("drawend", (event) => {
         const coordinates = transform(
           event.feature.getGeometry().getCoordinates(),
           "EPSG:3857",
           "EPSG:4326"
         );
-        console.log(coordinates);
-        this.$store.dispatch("getRevgeocode", coordinates.reverse());
+        this.$store
+          .dispatch("getRevgeocode", coordinates.reverse())
+          .then(() => {
+            event.feature.setProperties({
+              city: this.$store.state.city,
+              country: this.$store.state.country,
+            });
+            event.feature.setStyle(
+              new Style({
+                image: new Circle({
+                  stroke: new Stroke({
+                    color: "green",
+                  }),
+                  radius: 5,
+                }),
+                text: new Text({
+                  font: "12px Trebuchet MS",
+                  text: event.feature.get("city"),
+                  scale: 1.2,
+                  textBaseline: "bottom",
+                  offsetY: -5,
+                }),
+              })
+            );
+          });
+        // this.map.addOverlay(overlay);
         // overlay.setPosition(coordinates);
         this.toggleEditMode();
       });
       draw.setActive(false);
       draw.set("name", "drawInteraction");
       this.map.addLayer(vectorLayer);
-      // this.map.addOverlay(overlay);
       this.map.addInteraction(draw);
     },
     toggleEditMode() {
@@ -98,5 +130,9 @@ div {
   width: 100%;
   height: calc(100% - 60px);
   position: fixed;
+}
+
+.modal {
+  color: black;
 }
 </style>
