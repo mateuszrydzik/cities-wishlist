f9312dac84ce1d493fe9d48bf26347270808cca5
mateuszrydzik <panmisterek@outlook.com>
2022-06-29 18:05:24 +0200

    added router

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
-import 'vuetify/styles'
-import { createApp } from 'vue'
-import { createVuetify } from 'vuetify'
-import App from './App.vue'
-import { loadFonts } from './plugins/webfontloader'
-import store from "./store/index.js"
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles";
+import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import App from "./App.vue";
+import { loadFonts } from "./plugins/webfontloader";
+import store from "./store/index.js";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import router from "./router";
 
-loadFonts()
+loadFonts();
 
-const app = createApp(App)
+const app = createApp(App);
+
 const vuetify = createVuetify({
   components,
   directives,
-})
+});
 
-
-app.use(vuetify)
-app.use(store)
-app.mount('#app')
+app.use(vuetify);
+app.use(store);
+app.use(router);
+app.mount("#app");
