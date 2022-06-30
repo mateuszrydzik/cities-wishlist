7e4d4ff76b830ba6aebc64977c353b8bffbdc48f
mateuszrydzik <panmisterek@outlook.com>
2022-06-30 18:47:26 +0200

    changed font color

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,19 +1,20 @@
 <template>
-    <v-toolbar
-    app
-    dark
-    color="green"
-    height="60px"
-    >
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
-    </v-toolbar>
+  <v-toolbar app light color="green" height="60px">
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
+  </v-toolbar>
 </template>
 
 <script>
-export default{
-    name: 'Toolbar',
-    props: {
-        title: String
-    }
-}
+export default {
+  name: "Toolbar",
+  props: {
+    title: String,
+  },
+};
 </script>
+
+<style scoped>
+.v-toolbar-title {
+  color: white;
+}
+</style>
