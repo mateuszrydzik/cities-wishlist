b19aab9909709c33814810938f208a0f2069cbd9
mateuszrydzik <panmisterek@outlook.com>
2022-06-30 18:46:15 +0200

    welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <h1 class="tagline">
+      Find all the <span>cities</span> you ever <span>wished</span> to visit
+    </h1>
+    <p class="description">Easy to use app that helps you plan future trips</p>
+    <v-card-actions class="justify-center">
+      <v-btn :rounded="0" min-width="300px" variant="outlined" color="green"
+        >login to use</v-btn
+      ></v-card-actions
+    >
+    <v-row>
+      <v-col>
+        <v-card
+          min-height="150px"
+          variant="outlined"
+          color="green"
+          title="Select"
+          text="Mark your travel destination on the map with a single click. It's
+            name will be automatically saved."
+        >
+        </v-card> </v-col
+      ><v-col>
+        <v-card
+          min-height="150px"
+          variant="outlined"
+          color="green"
+          title="Modify"
+          text="Add notes to your markers and access it's information in a pop-up."
+        >
+        </v-card
+      ></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+};
+</script>
+<style scoped>
+.tagline {
+  text-align: center;
+  font-size: 60px;
+  padding: 2%;
+}
+.description {
+  text-align: center;
+  font-size: 30px;
+  color: gray;
+  padding: 2%;
+}
+.v-btn {
+  margin-bottom: 20px;
+}
+span {
+  color: green;
+}
+</style>
