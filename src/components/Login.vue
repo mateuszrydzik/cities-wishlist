226fe74bd260acdbe8c5ecf0c2ac88e959aab66b
mateuszrydzik <panmisterek@outlook.com>
2022-06-30 18:47:15 +0200

    modified login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,7 +3,8 @@
     <v-row justify="center" align="center" dense>
       <v-col cols="12" sm="8" md="4" lg="4">
         <div align="center" class="font-weight-bold text-capitalize">
-          Login to access app
+          <h2>Log In to Cities Wishlist</h2>
+          <br />
         </div>
         <v-form ref="login">
           <v-text-field
@@ -11,7 +12,7 @@
             name="username"
             type="email"
             class="rounded-0"
-            v-model="input.username"
+            v-model="form.username"
           >
           </v-text-field>
           <v-text-field
@@ -19,16 +20,13 @@
             name="password"
             type="password"
             class="rounded-0"
-            v-model="input.password"
+            v-model="form.password"
           ></v-text-field>
-          <v-btn
-            class="rounded-0"
-            x-large
-            block
-            color="green"
-            @click="consoleLog()"
+          <v-btn class="rounded-0" x-large block color="green" type="submit"
             >Login</v-btn
           >
+          <br />
+          <p v-if="showError" id="error-message">{{ error_message }}</p>
         </v-form>
       </v-col>
     </v-row>
@@ -36,28 +34,43 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "Login",
   data: () => ({
-    input: {
+    form: {
       username: "",
       password: "",
     },
+    showError: false,
   }),
   methods: {
     checkLogin() {
-      if (
-        this.input.username == this.$parent.admAccount.username &&
-        this.input.password == this.$parent.admAccount.password
-      ) {
-        console.log("success!");
+      if (this.input.username == "admin" && this.input.password == "admin") {
+        this.$router.push("/app");
       } else {
-        console.log("fail");
+        this.error_message = "Wrong username or password";
       }
     },
-    consoleLog() {
-      console.log(this.$parent.username);
+    ...mapActions(["Login"]),
+    async submit() {
+      const User = new FormData();
+      User.append("username", this.form.username);
+      User.append("password", this.form.password);
+      try {
+        await this.Login(User);
+        this.$router.push("/app");
+        this.showError = false;
+      } catch (error) {
+        this.showError = true;
+      }
     },
   },
 };
 </script>
+<style scoped>
+#error-message {
+  color: red;
+}
+</style>
