6a1474e47e1023b718e6eb46312034b2b84474fe
Mateusz Rydzik <panmisterek@outlook.com>
2022-07-24 12:49:39 +0200

    import button

---
+++ src/components/ButtonImportCity.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="button">
+    <v-container>
+      <v-btn icon elevation="10" color="green" @click="emitImportPlace">
+        {{ icon }}
+      </v-btn>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonImportCity",
+  data: () => ({
+    icon: "pull",
+  }),
+  methods: {
+    emitImportPlace() {
+      this.$parent.importPlace();
+    },
+  },
+};
+</script>
+
+<style scoped>
+#button {
+  position: fixed;
+  right: 5%;
+  bottom: 15%;
+  z-index: 1;
+}
+</style>
