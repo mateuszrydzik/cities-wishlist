c5a3ab3377815a7eb134e303af92024baffe93a8
mateuszrydzik <panmisterek@outlook.com>
2022-08-02 12:34:04 +0200

    added routes

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,34 @@
-from flask import Flask, render_template, jsonify
-from flask_cors import cross_origin, CORS
+from flask_cors import CORS
+from flask import Flask, render_template, jsonify, request
+from flask_cors import cross_origin
 from models import database, User, Place
+from configmodule import TestingConfig
 from peewee import fn
+from shapely.geometry import shape
 import json
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
-# CORS(app)
+app.config.from_object(TestingConfig())
 
-database.init(app.config['DBNAME'], host=app.config['DBHOST'], user = app.config['DBUSER'], password = app.config['DBPASS'])
-# database.create_tables([User])
+CORS(app, resoucers={r"/*": {"origins": "*"}})
 
-@app.route('/login', methods=['GET', 'POST'])
+
+database.init(app.config['DBNAME'], host=app.config['DBHOST'],
+              user=app.config['DBUSER'], password=app.config['DBPASS'],
+              port=app.config['DBPORT'])
+
+
+def create_tables():
+    with database:
+        database.ceate_tables([User, Place])
+
+
+@app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/place')
-@cross_origin(origins=['http://localhost:3000'])
+
+@app.route('/places', methods=['GET'])
 def get_place():
     places = Place.select(
         fn.ST_AsGeoJSON(Place.geom).alias('geom'),
@@ -26,15 +38,33 @@ def get_place():
     for row in places:
         geom = row.pop('geom')
         features.append({
-            "type":"Feature",
-            "geometry":json.loads(geom),
-            "properties":row
+            "type": "Feature",
+            "geometry": json.loads(geom),
+            "properties": row
         })
     return jsonify({
-        "type":"FeatureCollection",
-        "features":features
-        })
+        "type": "FeatureCollection",
+        "features": features
+    })
+
+
+@app.route('/places', methods=['POST'])
+def add_place():
+    req = request.json
+    geometry = 'SRID=4326;{}'.format(shape(req['geom']).wkt)
+    Place.create(city=req['city'], country=req['country'],
+                 notes=req['notes'], geom=geometry)
+    return {"status": "added"}
+
+
+@app.route('/places/<int:point_id>', methods=['DELETE'])
+def delete_place(point_id):
+    if (point := Place.get_or_none(Place.id == point_id)) is None:
+        return {"message": "Point not found"}
+    else:
+        point.delete_instance()
+        return {"message": "Point removed"}
+
 
 if __name__ == '__main__':
-    debug=True
     app.run(debug=True)
