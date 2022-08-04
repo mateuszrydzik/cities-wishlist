0ce6abae641f5b7c680a88ac6ef44a549f0b0ac3
mateuszrydzik <panmisterek@outlook.com>
2022-08-04 11:23:16 +0200

    syntax

--- backend/models.py
+++ backend/models.py
@@ -3,24 +3,28 @@ from peewee import PostgresqlDatabase, Model, PrimaryKeyField, TextField, Field
 
 database = PostgresqlDatabase(None)
 
+
 class GeometryField(Field):
     field_type = 'geometry'
     db_field = 'geometry'
 
+
 class AbstractModel(Model):
+    id = PrimaryKeyField()
+
     class Meta:
         database = database
 
+
 class User(AbstractModel):
-    id = PrimaryKeyField()
     name = TextField(unique=True)
     password = TextField()
 
     class Meta:
         db_table = 'user'
 
+
 class Place(AbstractModel):
-    id = PrimaryKeyField()
     city = TextField()
     country = TextField()
     notes = TextField()
