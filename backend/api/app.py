ea62fa55a37868c57bad8cceab6555515406e892
mateuszrydzik <panmisterek@outlook.com>
2022-08-25 18:17:25 +0200

    first swagger attempt

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,19 +1,57 @@
 from flask_cors import CORS
 from flask import Flask
 from flasgger import Swagger
-
 from api.config import create_db, create_tables, TestingConfig
 from api.views import places_bp
 
 
 def create_app() -> Flask:
 
+    template = {
+        "swagger": "2.0",
+        "info": {
+            "title": "My API",
+            "description": "API for my data",
+            "contact": {
+                "responsibleOrganization": "ME",
+                "responsibleDeveloper": "Me",
+                "email": "me@me.com",
+                "url": "www.me.com",
+            },
+            "termsOfService": "http://me.com/terms",
+            "version": "0.0.1"
+        },
+        "host": "mysite.com",  # overrides localhost:500
+        "basePath": "/docs",  # base bash for blueprint registration
+        "schemes": [
+            "http",
+            "https"
+        ],
+        "operationId": "getmyData"
+    }
+
+    swagger_config = {
+        "headers": [
+        ],
+        "specs": [
+            {
+                "endpoint": 'docs',
+                "route": '/docs.json',
+                "rule_filter": lambda rule: True,  # all in
+                "model_filter": lambda tag: True,  # all in
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        # "static_folder": "static",  # must be set by user
+        "swagger_ui": True,
+        "specs_route": "/docs"
+    }
     app = Flask(__name__)
     app.config.from_object(TestingConfig())
     CORS(app)
     app._db = create_db(app.config)
     create_tables(app._db)
-    app._swagger = Swagger(app)
+    app._swagger = Swagger(app, template=template, config=swagger_config)
     app.register_blueprint(places_bp)
 
     return app
