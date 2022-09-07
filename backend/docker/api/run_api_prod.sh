ce988917348fa219d06a740a4866b2216c40ec1a
mateuszrydzik <panmisterek@outlook.com>
2022-09-07 19:38:11 +0200

    script for production

---
+++ backend/docker/api/run_api_prod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export $(cat .env | xargs)
+
+until pg_isready -q -p 5432 -h ${CONTAINER_NAME}'_db' -U ${POSTGRES_USER};
+do
+    echo "Waiting for Postgres"
+    sleep 2;
+done;
+echo "Connected"
+cd app && python3 run_prod.py
