f60a34dd19f44ee3b3213fc13bebf0c9ee66d5a7
mateuszrydzik <panmisterek@outlook.com>
2022-09-08 18:32:06 +0200

    appbar component

--- frontend/src/components/Appbar.vue
+++ frontend/src/components/Appbar.vue
@@ -2,8 +2,6 @@
   <v-app-bar app light color="green" height="60px">
     <v-toolbar-title>Cities Wishlist</v-toolbar-title>
     <v-spacer></v-spacer>
-    <div>helo</div>
-    <div>hi</div>
   </v-app-bar>
 </template>
 
