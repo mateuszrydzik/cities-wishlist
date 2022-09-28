9c01510e92611b63bf83fbd468c7c2100cf02691
mateuszrydzik <panmisterek@outlook.com>
2022-09-28 15:59:33 +0200

    new welcome page layout

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -7,17 +7,49 @@
     <p class="description">
       Easy to use app that helps you plan your future trips
     </p>
-    <v-card-actions class="justify-center">
+    <div class="text-center">
       <v-btn
         :rounded="0"
         min-width="300px"
         min-height="60px"
-        variant="outlined"
         color="green"
         @click="App()"
         >launch app</v-btn
-      ></v-card-actions
+      >
+    </div>
+    <v-row align="center" justify="center">
+      <v-col>
+        <div class="text-h4 text-center" id="mainFeatures">
+          With <span>Cities Wishlist</span>, you can:
+        </div>
+      </v-col>
+      <v-col>
+        <v-carousel
+          cycle
+          height="300"
+          hide-delimiter-background
+          :show-arrows="false"
+        >
+          <v-carousel-item v-for="(color, i) in colors" :key="color">
+            <v-sheet height="100%" tile>
+              <div class="d-flex fill-height justify-center align-center">
+                <div class="text-h2">
+                  {{ color }}
+                </div>
+              </div>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+
+    <div
+      class="text-h3 text-center"
+      style="margin-bottom: 50px; margin-top: 20px"
     >
+      main features
+    </div>
+
     <v-row align="center" justify="center">
       <v-col>
         <v-card
@@ -25,7 +57,7 @@
           variant="outlined"
           color="green"
           title="Select"
-          text="Mark your travel destination on the map with a single click. City's name will be automatically saved!"
+          text="Browse the map, searching for your travel destination. When you're ready, press the plus button and mark it! City's name and it's country will be automatically saved!"
         >
         </v-card>
       </v-col>
@@ -42,7 +74,7 @@
           variant="outlined"
           color="green"
           title="Modify"
-          text="Add notes to your markers and access it's information in a pop-up."
+          text="Add notes to your cities and plan your trip ahead! With an edit feature, you can modify existing points, to change their notes"
         >
         </v-card></v-col
     ></v-row>
@@ -53,22 +85,74 @@
           variant="outlined"
           color="green"
           title="Save"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id ipsum bibendum, feugiat ligula vitae, molestie nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas quis neque sed leo blandit consectetur nec ut ligula.
-"
+          text="All your cities will be saved and accesible in the future! You don't need to worry about losing progress"
         >
         </v-card
       ></v-col>
       <v-col>
-        <v-img max-height="100px" src="./edit.png"></v-img>
+        <v-img max-height="100px" src="./save.png"></v-img>
+      </v-col>
+    </v-row>
+    <div class="text-center">
+      <v-btn
+        :rounded="0"
+        min-width="300px"
+        min-height="60px"
+        color="green"
+        @click="App()"
+        style="margin-top: 20px"
+        >test the features</v-btn
+      >
+    </div>
+    <div
+      class="text-h3 text-center"
+      style="margin-bottom: 20px; margin-top: 20px"
+      id="contact"
+    >
+      you can reach me on
+    </div>
+    <v-row>
+      <v-col>
+        <div class="text-h5 text-center" style="height: 80px">
+          GitHub
+
+          <a href="https://github.com/mateuszrydzik" target="_blank"
+            >mateuszrydzik</a
+          >
+        </div>
+      </v-col>
+      <v-col>
+        <div class="text-h5 text-center" style="height: 80px">
+          Mail
+
+          <a href="mailto:matrydzik@gmail.com" target="_blank"
+            >matrydzik@gmail.com</a
+          >
+        </div>
       </v-col>
     </v-row>
   </v-container>
+
+  <v-footer class="d-flex flex-column pa-0">
+    <div class="px-4 py-2 bg-black text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>mateusz rydzik</strong>
+    </div>
+  </v-footer>
 </template>
 
 <script>
 import Appbar from "./Appbar.vue";
 
 export default {
+  data: () => ({
+    colors: [
+      "search the world for inspirations",
+      "plan all your activities",
+      "save your trip ideas",
+      "pinpoint all previously visited locations",
+      "organise your holidays",
+    ],
+  }),
   name: "Welcome",
   components: {
     Appbar,
@@ -93,12 +177,20 @@ export default {
   padding: 2%;
 }
 .v-btn {
-  margin-bottom: 20px;
+  margin-bottom: 50px;
 }
 span {
   color: green;
 }
 .v-container {
   margin-top: 60px;
+}
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.9;
+  position: absolute;
+  width: 100%;
 }
 </style>
