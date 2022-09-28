c07b86fabe3eb82c3cf0369db1d251d102aca434
mateuszrydzik <panmisterek@outlook.com>
2022-09-28 15:59:05 +0200

    added buttons to app bar

--- frontend/src/components/Appbar.vue
+++ frontend/src/components/Appbar.vue
@@ -1,14 +1,20 @@
 <template>
   <v-app-bar app light color="green" height="60px">
     <v-toolbar-title>Cities Wishlist</v-toolbar-title>
-    <v-spacer></v-spacer>
+    <v-btn @click="emitApp()">Launch map</v-btn>
+    <v-btn href="#mainFeatures"> Features </v-btn>
+    <v-btn href="#contact"> Contact </v-btn>
   </v-app-bar>
 </template>
 
 <script>
 export default {
   name: "Appbar",
-  methods: {},
+  methods: {
+    emitApp() {
+      this.$parent.App();
+    },
+  },
 };
 </script>
 
